a6b5d780284da78984c8b7c32f110070e93b4257
devjoshixo <djoshi923@gmail.com>
2023-10-30 15:55:49 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import styles from "./App.module.css";
-import Sidepanel from "./Components/sidepanel/Sidepanel";
-import Body from "./Components/body/Body";
+import styles from './App.module.css';
+import Sidepanel from './Components/sidepanel/Sidepanel';
+import Body from './Components/body/Body';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Leaderboard from './Components/Leaderboard/Leaderboard';
 // import Header from "./Components/body/Header/Header";
 
 function App() {
   return (
     <div className={styles.app}>
       {/* <Header></Header> */}
-      <Sidepanel></Sidepanel>
-      <Body></Body>
+      {/* <Sidepanel />
+      <Body /> */}
+      <Sidepanel />
+      <Switch>
+        <Route path='/' exact>
+          <Body />
+        </Route>
+        <Route path='/leaderboard' exact>
+          <Leaderboard />
+        </Route>
+      </Switch>
     </div>
   );
 }
