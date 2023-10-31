4fbc4e9b433986599f2111804abeab8c55547b32
devjoshixo <djoshi923@gmail.com>
2023-10-31 14:06:16 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import styles from "./App.module.css";
-import Sidepanel from "./Components/sidepanel/Sidepanel";
-import Body from "./Components/body/Body";
-import Leaderboard from "./Components/Leaderboard/Leaderboard";
+import styles from './App.module.css';
+import Sidepanel from './Components/sidepanel/Sidepanel';
+import Body from './Components/body/Body';
+import Leaderboard from './Components/Leaderboard/Leaderboard';
+import Login from './Components/Login/Login';
+import {
+  Route,
+  Switch,
+  useLocation,
+} from 'react-router-dom/cjs/react-router-dom.min';
+import { useEffect, useState } from 'react';
 // import Header from "./Components/body/Header/Header";
 
 function App() {
+  const [showPanel, setShowPanel] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location);
+    if (location.pathname === '/login') {
+      setShowPanel(false);
+    } else {
+      if (!showPanel) {
+        setShowPanel(true);
+      }
+    }
+  }, [location]);
   return (
-    <div className={styles.app}>
-      {/* <Header></Header> */}
-      {/* <Sidepanel />
-      <Body /> */}
-      <Sidepanel />
-      <Body />
+    <div className={`${styles.app}  ${showPanel ? ' ' : styles.loginbg}`}>
+      {showPanel && (
+        <>
+          <Body />
+          <Sidepanel />
+        </>
+      )}
+      <Switch>
+        <Route path='/login' exact>
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Login.module.css';
+
+const Login = () => {
+  const submitButtonHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      <form className={styles.login}>
+        <h1>Login Page</h1>
+        <input type='text' placeholder='Username' />
+        <input type='password' placeholder='Password' />
+        <button onClick={submitButtonHandler}>Login</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
