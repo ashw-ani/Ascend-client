366eb765a0b18b3633f92a715e40b757263b5229
eternal-will <shuklashashwat2003@gmail.com>
2023-12-07 18:21:26 +0530

    course progress bar in platinum and diamond

--- src/Components/courses/Platinum.js
+++ src/Components/courses/Platinum.js
@@ -1,8 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./courses.module.css";
-import { useState } from "react";
 import getCourses from "../../api/getCourses";
-import { useEffect } from "react";
 import CourseTitle from "./CourseTitle/CourseTitle";
 import { ReactComponent as Loader } from "../../assets/signInButton.svg";
 import Progressindiactor from "../UI/course progress indicator/Progressindicator";
@@ -10,14 +8,36 @@ import Progressindiactor from "../UI/course progress indicator/Progressindicator
 function Platinum(props) {
   const [courses, setCourses] = useState(null);
   const [loader, setLoader] = useState(false);
-  const [completedProgress, setCompletedProgress] = useState(50);
+  const [totalLectures, setTotalLectures] = useState(0);
+  const [completeLectures, setCompleteLectures] = useState(0);
+  const [completedProgress, setCompletedProgress] = useState(70);
 
   useEffect(() => {
     const fetchCourses = async () => {
       setLoader(true);
-      const { courses } = await getCourses("Platinum");
+      const data = await getCourses("Platinum");
       setLoader(false);
-      setCourses(courses);
+      setCourses(data.courses);
+      let total_lectures = 0;
+      for (const course of data.courses) {
+        total_lectures += course.total_lectures;
+      }
+
+      setTotalLectures(total_lectures);
+
+      let completed_lectures = 0;
+
+      for (const course of data.courses) {
+        if (course.lecturesWatched) {
+          completed_lectures += course.lecturesWatched;
+        }
+      }
+
+      setCompleteLectures(completed_lectures);
+
+      setCompletedProgress(
+        parseInt((completed_lectures / total_lectures) * 100)
+      );
     };
 
     fetchCourses();
@@ -31,22 +51,24 @@ function Platinum(props) {
     );
   }
 
-  if (!courses) {
-    return null;
-  }
-
   return (
-    <div className={styles.courses_body}>
-      <Progressindiactor
-        bgcolor="orange"
-        progress={completedProgress}
-        height={15}
-      />
+    <>
+      {courses && (
+        <div className={styles.courses_body}>
+          <Progressindiactor
+            bgcolor="orange"
+            progress={completedProgress}
+            height={15}
+            lecturetotal={totalLectures}
+            lectureCompleted={completeLectures}
+          />
 
-      {courses.map((course) => (
-        <CourseTitle key={course.id} courseData={course} />
-      ))}
-    </div>
+          {courses.map((course) => (
+            <CourseTitle key={course.id} courseData={course} />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
 
