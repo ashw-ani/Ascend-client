f39b944a6fcd3a92804d9ebee2e83dc3be4863ae
eternal-will <shuklashashwat2003@gmail.com>
2023-12-29 19:13:49 +0530

    crystalline

--- src/api/fetchCrystallChallenge.js
+++ src/api/fetchCrystallChallenge.js
@@ -1,7 +1,7 @@
 const Getranks = async (reqtype) => {
   //   console.log(process.env.REACT_APP_API_URL);
   const Data = await fetch(
-    "https://script.google.com/macros/s/AKfycbzgZ9YJJK_2iqn7Iu5V6f99Yj2mVAWqyVDVYS8L5cbq8nvqukXJsf-H88tQmqN9VF-RJA/exec?reqtype=alltime"
+    "https://script.google.com/macros/s/AKfycbzZFyXCofgPiNKCUn3ueZ8GGnCWxQqvesrmqmrEGivj0UYSIHnYQWh1cTWRC6M8PhcF/exec?reqtype=alltime"
   );
   const result = await Data.json();
 
